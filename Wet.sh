9ec88301eb79ae11d3b69c1aecec974071b665e7
Sameeh Jubran <sameeh@gmail.com>
2016-01-15 19:28:43 +0200

    HW3: Registeration added.
    Signed-off-by: Sameeh Jubran <sameeh@gmail.com>

--- Wet.sh
+++ Wet.sh
@@ -1,24 +1,1 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+use 236363myDBdb.createCollection('results')var date = Date()var post={    "Res":1,    "ID1":123456789,    "ID2":123456789,    "Date":date}db.results.insert(post)db.results.find()
